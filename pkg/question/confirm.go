6ddd7d34a8a1683ba07d50a17f928d581b570c50
mliev <service@mliev.com>
2025-10-06 23:02:22 +0800

    fix(confirm): 优化确认输入的提示信息与验证逻辑

--- pkg/question/confirm.go
+++ pkg/question/confirm.go
@@ -15,7 +15,16 @@ func NewConfirm() *Confirm {
 }
 
 func (q *Confirm) Execute(question string, def any) any {
-	color.Print(question)
+	if strings.Contains(question, "</>") {
+		color.Print(question)
+	} else {
+		color.Info.Print(question)
+	}
+	color.Info.Print(" [")
+	color.LightGreen.Print("Y")
+	color.Info.Print("es/")
+	color.LightRed.Print("N")
+	color.Info.Print("o] : ")
 	input, _ := q.readInput()
 	return q.validateInput(input, def.(bool))
 }
@@ -23,19 +32,24 @@ func (q *Confirm) Execute(question string, def any) any {
 func (q *Confirm) readInput() (string, error) {
 	var input string
 	_, err := fmt.Scanln(&input)
+	color.Println()
 	return input, err
 }
 
-func (q *Confirm) validateInput(input string, def bool) any {
+func (q *Confirm) validateInput(input string, defaultValue bool) any {
 	if input == "" {
-		return def
+		return defaultValue
 	}
 	switch strings.ToLower(input) {
 	case "y":
 		return true
+	case "yes":
+		return true
 	case "n":
 		return false
+	case "no":
+		return false
 	default:
-		return q.Execute("\n<warn>无效输入，请重新输入 (y/n): </>", def)
+		return q.Execute("<warn>无效输入，请重新输入</>", defaultValue)
 	}
 }
